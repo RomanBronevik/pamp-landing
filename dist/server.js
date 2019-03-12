b0e88d258b106eace2896161572427938db49656
Roman <roman.bronevik@ya.ru>
2019-03-12 17:43:37 +0300

    dist fro heroku

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,7 @@
 let express = require('express')
 let serveStatic = require('serve-static')
 let app = express()
-app.use(serveStatic(__dirname))
+app.use(serveStatic(__dirname, 'dist'))
 let port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started ' + port)
