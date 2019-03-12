e0f552cc560b405f7cc2fa7f3cabb6139ca374ab
Roman <roman.bronevik@ya.ru>
2019-03-12 18:06:30 +0300

    remove favicon

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,8 @@
 let express = require('express')
 let serveStatic = require('serve-static')
+const path = require('path')
 let app = express()
-app.use(serveStatic(__dirname, 'dist'))
+app.use(serveStatic(path.join(__dirname, 'dist')))
 let port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started ' + port)
